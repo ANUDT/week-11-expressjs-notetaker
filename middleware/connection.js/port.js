758f9c8326beb3ee8409abf625c84dff64d127b6
ADT1 <152813453+ANUDT@users.noreply.github.com>
2024-04-20 23:20:08 +0100

    comment port.js

--- middleware/connection.js/port.js
+++ middleware/connection.js/port.js
@@ -3,3 +3,5 @@
     dialect; 'postgres',
     port; "5433";
 }
+
+//* this is wrongly here , this would be config Sequelize and PostgreSQL to database on localhost on port 5433 (my port is differemnt)
