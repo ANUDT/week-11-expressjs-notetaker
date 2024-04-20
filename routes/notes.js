f9963d4e1ae87a5ae5bd53d68c58033cdb129ec2
ADT1 <152813453+ANUDT@users.noreply.github.com>
2024-04-20 15:15:20 +0100

    notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,26 @@
-[
-  {
-    "email": "JohnAppleseed@2u.com",
-    "notesType": "UI",
-    "notes": "This site is cool",
-    "notes_id": "3fe5d166-2f9e-4e3d-87a7-25a561207c6c"
-  }
-]
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid'):
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+  } = require('../helpers/fsUtils');
+
+  notes.get('/', (req, res) => {
+    const noteId =req.params.note_id;
+    readFromFile('.db/db/.json')
+    .then((data)) => {
+    res.json(JSON.parse(data));
+  });
+
+  notes.get('/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
+    readFromFile('.db/db/.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.tip_id === tipID);
+      return result.lenght > 0
+      ? res.json(result)
+      :res.json ('no ID for note');
+    res.json(JSON.parse(data));
+  });
