5c8f1c13d87188b281fc2c8114f5b2a1f0c7e35d
ADT1 <152813453+ANUDT@users.noreply.github.com>
2024-04-20 22:29:57 +0100

    routes amended

--- routes/notes.js
+++ routes/notes.js
@@ -1,58 +1,64 @@
-const notes = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
+const notes = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
 const {
   readFromFile,
   readAndAppend,
   writeToFile,
-  } = require('../helpers/fsUtils');
+} = require("../helpers/fsUtils");
 
-  notes.get('/', (req, res) => {
-    const noteId = req.params.note_id;
-    readFromFile('.db/db/.json')
-    .then((data) => res.json(JSON.parse(data))
-  });
+// GET Route for retrieving all the tips
+notes.get("/", (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
 
-  notes.get('/:note_id', (req, res) => {
-    const noteId = req.params.note_id;
-    readFromFile('.db/db/.json')
+// GET Route for a specific tip
+notes.get("/:note_id", (req, res) => {
+  readFromFile("./db/db.json")
     .then((data) => JSON.parse(data))
-    .then((json) => { 
-      const result = json.filter((note) => note.tip_id === tipID);
-      return result.lenght > 0
-      ? res.json(result)
-      :res.json ('no ID for note');
-    res.json(JSON.parse(data));
-  })
-
-  notes.delete('/:note_id', (req, res) => {
-    const noteId = req.params.note_id;
-    readFromFile('.db/db/.json')
-    .then((data) => JSON.parse(data))
-    .then ((json) => {
-     
-      const result =json.filter(note.id !== noteId);
-
-      writeToFile('.db/db/.json',result);
-      res.json('Item${noteId} removed');
+    .then((json) => {
+      const result = json.filter((note) => note.id === tipId);
+      return result.length > 0
+        ? res.json(result)
+        : res.json("No note with that ID");
     });
-  });
+});
 
-  notes.post('/', (req, res) => {
-    console.log(req.body);
+// DELETE Route for a specific tip
+notes.delete("/:note_id", (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all notes except the one with the ID provided in the URL
+      const result = json.filter((note) => note.id !== noteId);
 
-    const{ title,text} = req.body;
+      // Save that array to the filesystem
+      writeToFile("./db/db.json", result);
 
-    if (req.body) {
-      const newNote ={
-        title,
-        text,
-        id: uuidv4(),
-      };
+      // Respond to the DELETE request
+      res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
+});
 
-      readAndAppend(newNote,'./db/db.json');
-      res.json('Added note succesfully');
-    } else {
-      res.error('Adding note unsuccesfull');
+// POST Route for a new UX/UI note
+notes.post("/", (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  console.log(req.body);
+  const { title, text } = req.body;
 
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
 
-    }
+    readAndAppend(newNote, "./db/db.json");
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error("Error in adding Note");
+  }
+});
+
+module.exports = notes;
--- server.js
+++ server.js
@@ -1,33 +1,25 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const api_routes = require("./routes/index.js");
+
 const app = express();
-const html_routes = require('./routes/html-routes')
-const api_routes = require('./routes/api-routes.js');
-const { clog } = require( './middleware/clog.js');
 
 const PORT = process.env.port || 3001;
-const fs =require('fs');
-
-app.use(clog);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
-app.use("./routes/diagnostics.json");
-app.use(html_routes);
-app.use(api_routes);
 
-app.get('/', (req, res) =>
-  res.sendFile('Navigate to/send or/routes visit http://localhost:3001/api'));
+app.use("/api", api_routes);
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/sendFile.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
 );
 
-app.get('/routes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/path.html'))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
 app.listen(PORT, () => {
-  console.log(`App listening at http://localhost:${PORT}`)
+  console.log(`App listening at http://localhost:${PORT}`);
 });
