00437181e3d76fddbd887bbc4bed8e640c438b74
ADT1 <152813453+ANUDT@users.noreply.github.com>
2024-04-21 20:39:25 +0100

    comments on feedback.js

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,15 +1,16 @@
 const fb = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+//* this are the Express functions and modules that will be used
 
-
+//* this code is used for apis to retrieve from server via http method
 fb.get('/', (req, res) =>
   readFromFile('./db/feedback.json')
   .then((data) => 
   res.json(JSON.parse(data)))
 );
 
-
+//* here the code creates a request with feedback with JSON, error message will appear is this unsuccesful
 fb.post('/', (req, res) => {
 
   const { email, feedbackType, feedback } = req.body;
