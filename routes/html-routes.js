97a8207ddcd681174f96dd528ab3e2eca2d201f4
ADT1 <152813453+ANUDT@users.noreply.github.com>
2024-04-21 20:49:27 +0100

    html-routes.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
 const path = require('path');
+//* this are express modules to define the HTTP routes -method in app
 
+//* set up of route in Node.js and express. below router modules has routes
 router
 const tipsRouter = require('./api-routes');
 const feedbackRouter = require('./feedback.js');
 const diagnosticsRouter = require('./diagnostics.json');
 
 
-
+//* per router module there is a route defination for going throught tips, feedback and diagnostic
 router.use('/tips', tipsRouter);
 router.use('/feedback', feedbackRouter);
 router.use('/diagnostic', diagnosticRouter);
